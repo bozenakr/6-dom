1de8ee83b9443fc6475a508021facdcc4f04f663
bozenakr <bozena.krasevic@gmail.com>
2022-11-12 20:28:28 +0200

    kebabai

---
+++ js/render.js
@@ -0,0 +1,47 @@
+const kebabai = [
+    // {
+    //     size: 'didelis/mazas',
+    //     main: 'jautiena/vistiena',
+    //     sauce: 'cesnakinis/mix/pikantiskas',
+    // },
+    {
+        size: 'didelis',
+        main: 'jautiena',
+        sauce: 'cesnakinis',
+    },
+    {
+        size: 'mazas',
+        main: 'vistiena',
+        sauce: 'mix',
+    },
+    {
+        size: 'mazas',
+        main: 'nesvarbu',
+        sauce: 'pikantiskas',
+    },
+];
+
+//pirma raide didele - paimam stringo [0] dedam uppercase ir darom slice(1) - praskipinam viena raide
+function formatWord(str) {
+    const up = str[0].toUpperCase() + str.slice(1);
+    return up;
+}
+//arrow nes tik vienas statement
+// const formatWord = s => s[0].toUpperCase() + s.slice(1);
+
+let HTML = '';
+
+for (const kebabas of kebabai) {
+    HTML += `<li>${formatWord(kebabas.size)} su ${kebabas.main} ir <span class="red">${kebabas.sauce}</span></li>`;
+}
+
+const kebabaiDOM = document.querySelector('#kebabai > ul');
+
+kebabaiDOM.innerHTML = HTML;
+
+const liDOM = kebabaiDOM.querySelectorAll('li');
+console.log(liDOM);
+
+for (const singleKebebasDOM of liDOM) {
+    singleKebebasDOM.classList.add('big');
+}
