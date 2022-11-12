9390c9d6de0e641be8a41fd20e1573dbb3a88a66
bozenakr <bozena.krasevic@gmail.com>
2022-11-12 20:02:06 +0200

    add remove toggle

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// // const h1DOM = document.getElementById('main-title');
 // const ulDOM = document.getElementsByTagName('ul')[0];
 // const liDOM = ulDOM.getElementsByTagName('li');
 
@@ -10,17 +9,34 @@
 
 
 
-const liDOM = document.querySelectorAll('ul > li');
+// // const liDOM = document.querySelectorAll('ul > li');
 
 
-for (let i = 0; i < liDOM.length; i++) {
-    console.log(liDOM[i].innerText);
-}
-
-console.log(liDOM);
+// for (let i = 0; i < liDOM.length; i++) {
+//     console.log(liDOM[i].innerText);
+// }
 
 
-for (const liElement of liDOM) {
-    liElement.classlist.add('red');
-}
+// =============prideda class==========
+// for (const liElement of liDOM) {
+//     liElement.classList.add('red');
+// }
+
+// // console.log(liDOM);
+
+// =============atima class==========
+// for (const liElement of liDOM) {
+//     liElement.classList.remove('red');
+// }
+
+// ===duoda kas neturejo, atima kas turejo class==========
+// for (const liElement of liDOM) {
+//     liElement.classList.toggle('red');
+// }
+
+
+//html class prideta main-title, surandam ir pagal Id ppridedam ('red')
+const h1DOM = document.getElementById('main-title');
+h1DOM.classList.add('red');
+
 
